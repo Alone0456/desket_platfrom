7e5d3c1cf91ac247ac67630cb94b14203b64df59
MrTry-Zhao <117160816+MrTry-Zhao@users.noreply.github.com>
2023-04-11 17:19:20 +0800

    任务清单页面初始化

---
+++ src/main/java/tyut/selab/desktop/ui/todolist/user/userFace.java
@@ -0,0 +1,11 @@
+package tyut.selab.desktop.ui.todolist.user;
+
+import javax.swing.*;
+
+public class userFace extends JPanel {
+    public userFace(){
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+
+    }
+}
