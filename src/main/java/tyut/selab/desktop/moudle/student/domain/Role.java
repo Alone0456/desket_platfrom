415af41001927a8978189db61b3d9e3a275ffcba
fjh-why <120797607+fjh-why@users.noreply.github.com>
2023-04-14 10:44:33 +0800

    4.14

--- src/main/java/tyut/selab/desktop/moudle/student/domain/Role.java
+++ src/main/java/tyut/selab/desktop/moudle/student/domain/Role.java
@@ -1,5 +1,16 @@
 package tyut.selab.desktop.moudle.student.domain;
 
 public class Role {
+
+
+
+    public String getDuty() {
+        return duty;
+    }
+
+    public void setDuty(String duty) {
+        this.duty = duty;
+    }
+
     private String duty;
 }
--- src/main/java/tyut/selab/desktop/moudle/student/userdao/impl/UserDao.java
+++ src/main/java/tyut/selab/desktop/moudle/student/userdao/impl/UserDao.java
@@ -1,34 +1,96 @@
 package tyut.selab.desktop.moudle.student.userdao.impl;
 
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 import tyut.selab.desktop.moudle.student.domain.Role;
 import tyut.selab.desktop.moudle.student.domain.User;
 import tyut.selab.desktop.moudle.student.userdao.IUserDao;
+import tyut.selab.desktop.utils.MysqlConnect;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao implements IUserDao {
-    @Override
+    private QueryRunner qr = new QueryRunner();
     public User queryUserByAccount(String accountNumber, String password) {
-        return null;
+        Connection connection =null;
+        try {
+            String sql = "select * from user"+
+                    "where accounterNumber ="+accountNumber+",password"+password
+                    ;
+            connection = MysqlConnect.getConnection();
+            return (User) qr.query(connection,sql,new BeanListHandler<>(User.class));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
+
+
+
     }
 
     @Override
     public List<User> queryUser() {
-        return null;
+        Connection connection =null;
+        try {
+            String sql = "select * from actor";
+            connection = MysqlConnect.getConnection();
+            return qr.query(connection,sql,new BeanListHandler<>(User.class));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
+
     }
 
     @Override
     public User queryUserByStudentNumber(Integer studentNumber) {
-        return null;
+        Connection connection =null;
+        try {
+            String sql = "select * from user"+
+                    "where studentNumber ="+studentNumber
+                    ;
+            connection = MysqlConnect.getConnection();
+            return (User) qr.query(connection,sql,new BeanListHandler<>(User.class));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
+
     }
 
     @Override
     public User queryUserByStudentName(String name) {
-        return null;
+        Connection connection =null;
+        try {
+            String sql = "select * from user"+"where studentNumber ="+name;
+            connection = MysqlConnect.getConnection();
+            return (User) qr.query(connection,sql,new BeanListHandler<>(User.class));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
+
     }
 
     @Override
-    public int insertUser(User user) {
+    public int insertUser(User user) {  Connection connection =null;
+        try {
+            String sql = "insert into actor(account_number,password,name,gender,phone,register_time,role_id) value(?,?,?,?,?,?,?)";
+            connection = MysqlConnect.getConnection();
+
+            qr.query(connection,sql,new BeanListHandler<>(User.class));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            MysqlConnect.close(null,null,connection);
+        }
         return 0;
     }
 
